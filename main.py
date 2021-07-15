0d6cd4eb04ffd4830bf7d9c895394095402d859d
Igor Srdoč <igor@srdoc.si>
2021-07-15 18:18:19 +0200

    Initial commit. Add python data type examples

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hello! My name is Igor.")
+
+# Data types
+nothing = None
+is_life_good = True
+age = 35
+length = 100.45
+fruit = "apple 22"
+people = ["Janez", "Mojca"]
+course = {
+    "difficulty": "beginner",
+    "number_of_sessions": 24
+}
+
+print(fruit + " yeah")
+print(age + 14)
