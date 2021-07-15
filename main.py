b44c77117d74e1bd76729c138fb41c7adc2324b7
Igor Srdoč <igor@srdoc.si>
2021-07-15 18:28:09 +0200

    Add input string example

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-print("Hello! My name is Igor.")
+user_name = input("Enter your name: ")
 
-# Data types
-nothing = None
-is_life_good = True
-age = 35
-length = 100.45
-fruit = "apple 22"
-people = ["Janez", "Mojca"]
-course = {
-    "difficulty": "beginner",
-    "number_of_sessions": 24
-}
-
-print(fruit + " yeah")
-print(age + 14)
+print("Your name is: " + user_name)
