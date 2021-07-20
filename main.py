a40c4a3745d267a278a3a26f761308d2f03501f8
Igor Srdoč <igor@srdoc.si>
2021-07-20 18:15:36 +0200

    Add secret number game with while loop

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-for number in range(5):
-    print("The number is: " + str(number))
+secret = 12
+guess = None
+
+while True:
+    guess = int(input("Guess the secret number (between 1 and 30): "))
+
+    if guess == secret:
+        print("You've guessed it - congratulations! It's number " + str(secret) + ".")
+        break
+
+    print("Sorry, your guess is not correct... The secret number is not " + str(guess))
