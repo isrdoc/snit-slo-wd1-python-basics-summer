1e0ec06c6706b6760d8853be443ee66b2ae656ab
Igor Srdoč <igor@srdoc.si>
2021-07-20 17:31:37 +0200

    Add if/else mood example

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-user_age_number = int(input("Enter your age: "))
+print("Start")
 
-print(user_age_number + 10)
+mood = input("Tell me your mood: ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+    print("Yaay!")
+elif mood == "sad":
+    print("Sorry for you.")
+elif mood == "excited":
+    print("Woohoo!")
+else:
+    print("I don't know about that mood")
+
+print("End")
