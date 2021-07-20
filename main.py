938282b8477b96822ac8461946e5c12ed3f1e536
Igor Srdoč <igor@srdoc.si>
2021-07-20 18:02:48 +0200

    Add for loop with range of 5

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-did_consent = "no"
-
-while did_consent != "yes":
-    did_consent = input("Do you consent to the terms (yes/no)? ")
-    if did_consent != "yes":
-        print("Please do consent.")
+for number in range(5):
+    print("The number is: " + str(number))
