338b5a623706f64e28b7be88c2c89a45fcc21ace
Igor Srdoč <igor@srdoc.si>
2021-07-22 17:45:00 +0200

    Add list for loop example

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-cities = ["Ljubljana", "Maribor", "London", "Zagreb"]
-hours_at_which_i_got_up = [4, 6, 6, 3]
+car_brands = ["tesla", "toyota", "nissan"]
+car_brands.sort()
 
-for hour in hours_at_which_i_got_up:
-    print(f"I got up at {hour} hrs.")
-
-
-basket_list = ["apple", "phone"]
-basket_dict = {
-    "size": "large",
-    "color": "orange",
-    "is_full": False,
-    "serial_number": 231423545,
-    "items": ["apple", "phone"],
-    "handle": {
-        "material": "straw",
-        "durability": 40
-    }
-}
+for brand in car_brands:
+    print(f"I like my {brand}.")
